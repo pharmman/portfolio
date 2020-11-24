0b634e271711833ec9000d8873e46b24ecbeab1a
Pharmman <pharm.sale777@gmail.com>
2020-11-24 22:44:21 +0300

    In progress

--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import styleContainer from '../common/styles/Container.module.css'
-import {Skill} from "../skill/skill";
+import {Skill} from "./skill/Skill";
 import styles from './Skills.module.css'
 
 
 export const Skills = () => {
     return (
         <div className={styles.skillsBlock}>
-            <div className={styleContainer.container}>
-                <div className={styles.title}>
-                <h2>My Skills</h2>
+            <div className={`${styleContainer.container} ${styles.skillsContainer}`}>
+                <h2 className={styles.title}>My Skills</h2>
+                <div className={styles.skills}>
+                    <Skill title={'JS'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. neque nulla officia, quibusdam similique.'}/>
+                    <Skill title={'CSS'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. rem saepe ullam? Doloribus, harum.'}/>
+                    <Skill title={'HTML'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ut? A consequuntur illo optio.'}/>
                 </div>
-                <Skill/>
-                <Skill/>
-                <Skill/>
             </div>
 
         </div>
