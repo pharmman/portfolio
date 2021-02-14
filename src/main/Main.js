eca4454553ead265ed45812844a17ad8c20da1ae
Pharmman <pharm.sale777@gmail.com>
2021-02-15 00:57:31 +0300

    main responsive in progress

--- src/main/Main.js
+++ src/main/Main.js
@@ -10,12 +10,12 @@ export const Main = () => {
                 <div className={styles.text}>
                     <h6>HI THERE !</h6>
                     <h1>I'M <span className={styles.name}>Aleksandr Rasskazov</span></h1>
-                    <p>Frontend Developer</p>
+                    <h2>Frontend Developer</h2>
                 </div>
-                <Tilt className="Tilt" options={{max: 25}}>
-                    <div className="Tilt-inner">
+                <Tilt className={'Tilt'} options={{max: 25}}>
+                    <div className={'Tilt-inner'}>
                         <div className={styles.photo}>
-                            <img src={myPhoto} alt="Me"/>
+                            <img src={myPhoto} alt="Me" />
                         </div>
                     </div>
                 </Tilt>
