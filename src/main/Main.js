cab6c25453fddfd1c041b572a2bf7551a9d792da
Pharmman <pharm.sale777@gmail.com>
2021-02-14 22:53:48 +0300

    main in progress

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import styles from './Main.module.scss'
-import styleContainer from '../common/styles/Container.module.scss'
+import myPhoto from '../assets/images/myPhoto.jpg'
+import Tilt from 'react-tilt/dist/tilt';
 
 export const Main = () => {
     return (
         <div className={styles.mainBlock}>
-            <div className={styleContainer.container}>
+            <div className={styles.container}>
                 <div className={styles.text}>
                     <h6>HI THERE !</h6>
                     <h1>I'M <span className={styles.name}>Aleksandr Rasskazov</span></h1>
                     <p>Frontend Developer</p>
                 </div>
-                    <div className={styles.photo}>
-                        Photo
+                <Tilt className="Tilt" options={{max: 25}}>
+                    <div className="Tilt-inner">
+                        <div className={styles.photo}>
+                            <img src={myPhoto} alt="Me"/>
+                        </div>
                     </div>
+                </Tilt>
+
             </div>
         </div>
     );
