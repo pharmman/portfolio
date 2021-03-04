80a7b972d59f60f09c5aad41aa9d193ddd61ab2c
Pharmman <pharm.sale777@gmail.com>
2021-03-05 01:03:14 +0300

    footer style changed

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import styles from './Footer.module.scss'
-import twitterIcon from '../assets/images/twitter.svg'
 import linkedinIcon from '../assets/images/linkedin.svg'
 import vkIcon from '../assets/images/vk.svg'
-import instagramIcon from '../assets/images/instagram.svg'
+import facebookIcon from '../assets/images/facebook.svg'
+import telegramIcon from '../assets/images/telegram.svg'
 
 export const Footer = () => {
     return (
@@ -13,8 +13,8 @@ export const Footer = () => {
                 <ul className={styles.listContacts}>
                     <li className={styles.contact}><a href={'1'}> <img alt={'#'} src={linkedinIcon} /></a></li>
                     <li className={styles.contact}><a href={'2'}> <img alt={'#'} src={vkIcon} /></a></li>
-                    <li className={styles.contact}><a href={'3'}> <img alt={'#'} src={instagramIcon} /></a></li>
-                    <li className={styles.contact}><a href={'4'}> <img alt={'#'} src={twitterIcon} /></a></li>
+                    <li className={styles.contact}><a href={'3'}> <img alt={'#'} src={facebookIcon} /></a></li>
+                    <li className={styles.contact}><a href={'4'}> <img alt={'#'} src={telegramIcon} /></a></li>
                 </ul>
                 <p>&copy; 2021 Все права защищены</p>
             </div>
