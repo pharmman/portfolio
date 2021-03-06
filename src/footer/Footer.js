cb08700eee0816f226eafdb51d671420dc1c3c09
Pharmman <pharm.sale777@gmail.com>
2021-03-07 00:47:23 +0300

    links to footer and redux skill

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -13,10 +13,10 @@ export const Footer = () => {
             <div className={styles.container}>
                 <h2 className={styles.title}>Rasskazov Aleksandr</h2>
                 <ul className={styles.listContacts}>
-                    <li className={styles.contact}><a href={'1'}> <img alt={'#'} src={linkedinIcon} /></a></li>
-                    <li className={styles.contact}><a href={'2'}> <img alt={'#'} src={vkIcon} /></a></li>
-                    <li className={styles.contact}><a href={'3'}> <img alt={'#'} src={facebookIcon} /></a></li>
-                    <li className={styles.contact}><a href={'4'}> <img alt={'#'} src={telegramIcon} /></a></li>
+                    <li className={styles.contact}><a href={'https://www.linkedin.com/in/aleksandr-rasskazov-57b661208/'}> <img alt={'#'} src={linkedinIcon} /></a></li>
+                    <li className={styles.contact}><a href={'https://vk.com/id34437088'}> <img alt={'#'} src={vkIcon} /></a></li>
+                    <li className={styles.contact}><a href={'https://www.facebook.com/profile.php?id=100037371967527'}> <img alt={'#'} src={facebookIcon} /></a></li>
+                    <li className={styles.contact}><a href={'https://t.me/Pharmman'}> <img alt={'#'} src={telegramIcon} /></a></li>
                 </ul>
                 <p>&copy; 2021 All rights reserved</p>
             </div>
--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -6,11 +6,12 @@ import {faCode} from '@fortawesome/free-solid-svg-icons';
 import {faJsSquare} from '@fortawesome/free-brands-svg-icons';
 import {faReact} from '@fortawesome/free-brands-svg-icons/faReact';
 import Fade from 'react-reveal/Fade';
+import {SkillWithoutFA} from './skillwithoutFA/SkillWithoutFA';
 
 export const Skills = () => {
     return (
-            <div id='skills' className={styles.skillsBlock}>
-                <Fade right cascade>
+        <div id='skills' className={styles.skillsBlock}>
+            <Fade right cascade>
                 <div className={styles.container}>
                     <h2 className={styles.title}>My <span>Skills</span> <ShadowTitle title={'Skills'}/></h2>
                     <div className={styles.skills}>
@@ -26,10 +27,13 @@ export const Skills = () => {
                                icon={faCode}
                                description={'Lorem ipsum dolor sit amet, consectetur ' +
                                'adipisicing elit. ut? A consequuntur illo optio.'}/>
+                        <SkillWithoutFA title={'Redux'}
+                               description={'Lorem ipsum dolor sit amet, consectetur ' +
+                               'adipisicing elit. ut? A consequuntur illo optio.'}/>
                     </div>
                 </div>
-                </Fade>
-            </div>
+            </Fade>
+        </div>
 
     )
 }
