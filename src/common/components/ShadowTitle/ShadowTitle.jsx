b215fcca32ec981246c7cd98f6e0f4969a3252a8
Pharmman <pharm.sale777@gmail.com>
2021-03-23 20:45:53 +0300

    second version

--- src/common/components/ShadowTitle/ShadowTitle.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from './ShadowTitle.module.scss'
-
-export const ShadowTitle = (props) => {
-    return <span className={styles.title}>{props.title}</span>
-}
