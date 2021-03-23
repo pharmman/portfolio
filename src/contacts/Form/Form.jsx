fc8dd1b1c510dcb3086d3e054ba524f68f3cf1ec
Pharmman <pharm.sale777@gmail.com>
2021-03-23 14:56:16 +0300

    answer text

--- src/contacts/Form/Form.jsx
+++ src/contacts/Form/Form.jsx
@@ -5,32 +5,41 @@ import axios from 'axios';
 
 export const Form = () => {
 
-     const [name, setName] = useState('')
-     const [email, setEmail] = useState('')
-     const [text, setText] = useState('')
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [text, setText] = useState('')
+    const [isSent, setIsSent] = useState(false)
+
 
     const handleSubmit = (e) => {
-         e.preventDefault()
-         axios.post('http://localhost:3010/sendMessage', {
-             name,
-             email,
-             text
-         })
-             .then(() => {
-                 //TODO change state for show another component
-                 alert('')
-             })
+        e.preventDefault()
+        axios.post('http://localhost:3010/sendMessage', {
+            name,
+            email,
+            text
+        })
+            .then(() => {
+                setIsSent(true)
+            })
     }
 
     return (
-        <Fade left cascade>
-        <form>
-            <input value={name} onChange={e => setName(e.currentTarget.value)} name="name" type="text" className={styles.feedbackInput} placeholder="Name"/>
-            <input value={email} onChange={e => setEmail(e.currentTarget.value)} name="email" type="text" className={styles.feedbackInput} placeholder="Email"/>
-            <textarea value={text} onChange={e => setText(e.currentTarget.value)} name="text" className={styles.feedbackInput} placeholder="Comment"/>
-            <input onClick={handleSubmit} type="submit" value="SEND EMAIL"/>
-        </form>
-        </Fade>
+        isSent ?
+            <Fade left>
+                <h2 className={styles.feedbackAnswer}>Thank you for your interest, I will contact you soon!</h2>
+            </Fade>
+            :
+            <Fade left cascade>
+                <form>
+                    <input value={name} onChange={e => setName(e.currentTarget.value)} name="name" type="text"
+                           className={styles.feedbackInput} placeholder="Name"/>
+                    <input value={email} onChange={e => setEmail(e.currentTarget.value)} name="email" type="text"
+                           className={styles.feedbackInput} placeholder="Email"/>
+                    <textarea value={text} onChange={e => setText(e.currentTarget.value)} name="text"
+                              className={styles.feedbackInput} placeholder="Comment"/>
+                    <input onClick={handleSubmit} type="submit" value="SEND EMAIL"/>
+                </form>
+            </Fade>
     )
 }
 
